d284a2f95bd49bf4dfcba24789e49b8969b03782
Tracy Smith <tracysuzanne321@gmail.com>
2022-07-12 19:10:14 +0100

    chore: set up custom colours in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
-		extend: {},
+		colors: {
+			blogistaBlue: '#7A9E9F',
+			blogistaRed: '#FE5F55',
+			blogistaBeige: '#EEF5DB',
+			blogistaDeepBlue: '#4F6367',
+			blogistaPowderblue: '#B8D8D8',
+		},
 	},
 	plugins: [],
 };
